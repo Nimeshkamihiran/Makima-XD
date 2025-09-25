c5c8320c7065df6244f258c4c6e40ee72358eaea
NIMESHKA MIHIRAN ❤️💪 <nimeshmihiranga183@gmail.com>
2025-09-25 19:19:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ function convertToBool(text, trueValue = 'true') {
 
 module.exports = {
   // ===== BOT CORE SETTINGS =====
-  SESSION_ID: process.env.SESSION_ID || "", // Your bot's session ID 
+  SESSION_ID: process.env.SESSION_ID || "suho~GQUkFTKa#IyzbUvPM86LAX3JsPESNrh9AdAfHDX3ye7hosNl5cYA", // Your bot's session ID 
   PREFIX: getConfig("PREFIX") || ",", // Command prefix (e.g., ., /, !, *)
   CHATBOT: getConfig("CHATBOT") || "on", // Chatbot mode: on/off
   BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "Makima-XD", // Bot display name
